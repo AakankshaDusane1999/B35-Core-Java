7f5c4e0109c4e5bd73820ee1367bce43a8cb8458
mrand <mrand@LAPTOP-FBEAVA6P>
2022-11-25 16:24:09 +0530

    1stcommit

---
+++ Day-3_oprator_And_Control-statement/src/operatordemo/Assingmentoperrator.java
@@ -0,0 +1,31 @@
+package operatordemo;
+
+import java.util.Scanner;
+
+public class Assingmentoperrator {
+
+	public static void main(String[] args) {
+
+		Scanner s = new Scanner(System.in);
+		System.out.println("Enter the value");
+		int a= s.nextInt();
+		int b= s.nextInt();
+		a*=b;
+		//a-=b;
+		//a+=b;
+		//a/=b;
+		//a%=b;
+		//System.out.println(a);
+		//System.out.println(a);	
+		//System.out.println(a);
+		System.out.println(a);
+		
+
+		s.close();
+		
+				
+			
+
+	}
+
+}
---
+++ Day-3_oprator_And_Control-statement/src/operatordemo/arthimaticoperator.java
@@ -0,0 +1,25 @@
+package operatordemo;
+
+import java.util.Scanner;
+
+public class arthimaticoperator {
+
+	public static void main(String[] args) 
+	{
+	
+Scanner s= new Scanner(System.in);
+System.out.println("Enter the value");
+int a =s.nextInt();
+int b =s.nextInt();
+
+System.out.println("The addition is"+(a+b));
+System.out.println("The subtraction is"+(a-b));
+System.out.println("The multification is"+(a*b));
+System.out.println("The divisino"+(a/b ));
+System.out.println("The modules is"+(a%b));
+s.close();				
+
+
+}
+}
+
