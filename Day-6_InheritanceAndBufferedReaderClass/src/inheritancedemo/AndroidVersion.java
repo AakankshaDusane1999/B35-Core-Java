799166862ad820397ecd1b0f984f7d95f093d6a1
mrand <mrand@LAPTOP-FBEAVA6P>
2022-11-28 13:32:59 +0530

    1st commit

---
+++ Day-6_InheritanceAndBufferedReaderClass/src/inheritancedemo/AndroidVersion.java
@@ -0,0 +1,14 @@
+package inheritancedemo;
+
+public class AndroidVersion {
+
+//demo on hierchical inheritance
+//parent class
+	
+	protected String name="Android version name";
+	public void display()
+	{
+		System.out.println(name);
+	}
+
+}
