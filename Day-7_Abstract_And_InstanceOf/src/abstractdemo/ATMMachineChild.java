c7b60bc4aee7319d0558588cbdb7a32a590ddd4c
mrand <mrand@LAPTOP-FBEAVA6P>
2022-12-02 15:40:50 +0530

    1st commit

---
+++ Day-7_Abstract_And_InstanceOf/src/abstractdemo/ATMMachineChild.java
@@ -0,0 +1,11 @@
+package abstractdemo;
+public class ATMMachineChild extends ATMMachine{
+
+	//provided the implementation to abstarct method of parent class
+	@Override
+	void withdraw() {
+		
+		int balance=150000;
+		System.out.println("Withdrawing amount is: "+balance);
+	}		
+}
