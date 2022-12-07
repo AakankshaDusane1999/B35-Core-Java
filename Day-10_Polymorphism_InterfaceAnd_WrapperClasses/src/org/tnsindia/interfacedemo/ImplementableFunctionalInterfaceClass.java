df19f0165fb43b34be0ef5e1ed5c1905d5b21249
mrand <mrand@LAPTOP-FBEAVA6P>
2022-12-07 13:42:32 +0530

    1st commit

---
+++ Day-10_Polymorphism_InterfaceAnd_WrapperClasses/src/org/tnsindia/interfacedemo/ImplementableFunctionalInterfaceClass.java
@@ -0,0 +1,11 @@
+package org.tnsindia.interfacedemo;
+
+
+public class ImplementableFunctionalInterfaceClass implements FunctionalInterfaceDemo{
+
+	public void print(String msg) {
+		System.out.println(msg);
+		
+	}
+
+}
