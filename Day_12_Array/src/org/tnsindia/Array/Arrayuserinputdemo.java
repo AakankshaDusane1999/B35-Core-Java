8ee94c4e5dca081353daf6607a5349d841bc5a26
mrand <mrand@LAPTOP-FBEAVA6P>
2022-12-10 16:47:07 +0530

    1st commit

---
+++ Day_12_Array/src/org/tnsindia/Array/Arrayuserinputdemo.java
@@ -0,0 +1,27 @@
+package org.tnsindia.Array;
+
+import java.util.Scanner;
+
+public class Arrayuserinputdemo {
+
+	public static void main(String[] args) {
+	Scanner s = new Scanner(System.in);
+	System.out.println("enter the size of arrey:  ");
+	int size=s.nextInt();
+	//array decleration
+	int arr[]=new int[size];
+	System.out.println("enter the valuein the array :  ");
+	
+	//entering value
+	for (int i=0;i<arr.length;i++) 
+	{   
+		System.out.println("enter the"+i+"th index");
+ 	    arr[i]=s.nextInt();
+	}
+	
+	for (int i=0;i<arr.length;i++)
+	System.out.println(arr[i]+"");
+	s.close();
+	}
+}
+	
